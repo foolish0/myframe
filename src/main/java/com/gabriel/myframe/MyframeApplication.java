5e829a69dac8fdcb44677cedc2bc8bcf41f86cd4
Gabriel <lizhenjiang.xidian@foxmail.com>
2022-04-05 16:05:30 +0800

    Gabriel - init project

---
+++ src/main/java/com/gabriel/myframe/MyframeApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.myframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyframeApplication.class, args);
+    }
+
+}
